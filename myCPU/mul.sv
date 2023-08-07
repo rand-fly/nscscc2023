04ddc0ab1b592781bc6ffbdf401ea0bc2f71c992
renfei147 <renfei8@live.cn>
2023-08-07 21:24:43 +0800

    把仿真模块整理到sim目录

--- myCPU/mul.sv
+++ myCPU/mul.sv
@@ -38,25 +38,3 @@ module mul (
   );
 
 endmodule
-
-// for verilator simulation
-`ifdef SIMU
-module mult_gen_0 (
-    input             CLK,
-    input      [32:0] A,
-    input      [32:0] B,
-    output reg [63:0] P
-);
-  logic [32:0] A_reg;
-  logic [32:0] B_reg;
-
-  always_ff @(posedge CLK) begin
-    A_reg <= A;
-    B_reg <= B;
-  end
-
-  always_ff @(posedge CLK) begin
-    P <= $signed(A_reg) * $signed(B_reg);
-  end
-endmodule
-`endif
--- myCPU/sim/blk_mem_gen_cache_32.sv
+++ myCPU/sim/blk_mem_gen_cache_32.sv
@@ -1,22 +1,22 @@
 // for verilator simulation
 `ifdef SIMU
 
-module blk_mem_gen_cache_32(
-  input wire clka,
-  input wire [0:0]wea,
-  input wire [6:0]addra,
-  input wire [255:0]dina,
-  output reg [255:0]douta
+module blk_mem_gen_cache_32 (
+    input wire clka,
+    input wire [0:0] wea,
+    input wire [6:0] addra,
+    input wire [255:0] dina,
+    output reg [255:0] douta
 );
 
-reg [255:0] mem [0:127];
+  reg [255:0] mem[0:127];
 
-always @(posedge clka) begin
+  always @(posedge clka) begin
     if (wea) begin
-        mem[addra] <= dina;
+      mem[addra] <= dina;
     end
     douta <= mem[addra];
-end
+  end
 
 endmodule
 
